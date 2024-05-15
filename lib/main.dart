21badb289516509d23ce531e5780e3ccf49e48e1
saisanjit <saisanjit.d@gmail.com>
2024-05-15 15:35:55 +0530

    Added tree view and split view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
+import 'package:animated_tree_view/animated_tree_view.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'package:split_view/split_view.dart';
+
+
+
+const showSnackBar = false;
+const expandChildrenOnReady = true;
 
 void main() {
   runApp(const MyApp());
@@ -11,115 +20,113 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Simple Animated Tree Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  MyHomePageState createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePageState extends State<MyHomePage> {
+  TreeViewController? _controller;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      floatingActionButton: ValueListenableBuilder<bool>(
+        valueListenable: sampleTree.expansionNotifier,
+        builder: (context, isExpanded, _) {
+          return FloatingActionButton.extended(
+            onPressed: () {
+              if (sampleTree.isExpanded) {
+                _controller?.collapseNode(sampleTree);
+              } else {
+                _controller?.expandAllChildren(sampleTree);
+              }
+            },
+            label: isExpanded
+                ? const Text("Collapse all")
+                : const Text("Expand all"),
+          );
+        },
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Padding(
+        padding: const EdgeInsets.only(top:16.0),
+        child: SplitView(
+          controller: SplitViewController(limits: [WeightLimit(max: 0.2),WeightLimit(max: 0.8)]),
+          gripSize: 6,
+          viewMode: SplitViewMode.Horizontal,
+          children: [
+            
+            
+            Column(children: [Text("Properties")],),
+        
+        
+        
+            TreeView.simple(
+              tree: sampleTree,
+              showRootNode: false,
+               
+              expansionBehavior: ExpansionBehavior.none,
+              
+              expansionIndicatorBuilder: (context, node) =>
+              NoExpansionIndicator(tree: node),
+                  
+              
+              indentation: const Indentation(style: IndentStyle.squareJoint),
+             
+              onItemTap: (item) {
+                if (kDebugMode) print("Item tapped: ${item.key}");
+                print("children ${item.childrenAsList}");
+              },
+        
+              onTreeReady: (controller) {
+                _controller = controller;
+                if (expandChildrenOnReady) controller.expandAllChildren(sampleTree);
+              },
+            builder: (context, node) => Row(
+          children: [
+            Expanded(
+        child: Card(
+          child: ListTile(
+            title: Text("Item ${node.level}-${node.key} - ${node.data}"),
+            subtitle: Text('Level ${node.level}'),
+          ),
+        ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            IconButton(
+        icon: Icon(Icons.add_circle_rounded), 
+        onPressed: () {
+          print("parent_key ${node.key}");
+          TreeNode newNode = TreeNode();
+          node..add(newNode);
+         print("newnode ${newNode.key}");
+        },
+            ),
+            IconButton(
+        icon: Icon(Icons.delete), // Add another icon for a different action
+        onPressed: () {node.parent!.remove(node);
+        },
+            ),
+          ],
+        ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+final sampleTree = TreeNode.root()..add(TreeNode(key: "0"));
