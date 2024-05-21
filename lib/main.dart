1508344c941af2391bb285f6393db9fbef321247
mobiledevnalsoft <mobiledevnalsoft@gmail.com>
2024-05-21 18:25:19 +0530

    added new package

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,21 @@
-import 'dart:ui_web';
-
-import 'package:animated_tree_view/animated_tree_view.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:json_dynamic_widget/json_dynamic_widget.dart';
-import 'package:mirai/mirai.dart';
 import 'package:provider/provider.dart';
-import 'package:server_driven_ui/UI/home_view.dart';
+import 'package:serve_dynamic_ui/serve_dynamic_ui.dart';
 import 'package:server_driven_ui/Providers/ui_provider.dart';
-import 'package:server_driven_ui/UI/dynamic_ui.dart';
-import 'package:server_driven_ui/UI/mirai_ui.dart';
-
-import 'package:split_view/split_view.dart';
+import 'package:server_driven_ui/UI/server_driven_ui.dart';
 
 const showSnackBar = false;
 const expandChildrenOnReady = true;
 
 void main() {
-  runApp(MaterialApp(home: MultiProvider(
-        providers: [ChangeNotifierProvider(create: (context) => UIProvider(),)],
-        child: const HomeView(title: 'Simple Animated Tree Demo')),));
-}
+  ServeDynamicUI.init();
 
+  runApp(MaterialApp(
+    home: MultiProvider(providers: [
+      ChangeNotifierProvider(
+        create: (context) => UIProvider(),
+      )
+    ], child: const ServerDrivenUI()),
+  ));
+}
