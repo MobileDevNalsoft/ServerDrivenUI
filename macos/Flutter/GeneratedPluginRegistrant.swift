6314f6a7cfcf5cb4954eee560ac70610a35cf8af
mobiledevnalsoft <mobiledevnalsoft@gmail.com>
2024-05-28 18:08:12 +0530

    .

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import extract
+import rive_common
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ExtractPlugin.register(with: registry.registrar(forPlugin: "ExtractPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <extract/extract_plugin_c_api.h>
+#include <rive_common/rive_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  ExtractPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ExtractPluginCApi"));
+  RivePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("RivePlugin"));
 }
